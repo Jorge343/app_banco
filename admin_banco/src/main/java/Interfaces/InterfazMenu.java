27dff565e4d011458f2f0650f2ba36a5b15f7101
Jorge343 <jorge.gaitan.343@gmail.com>
2026-04-15 16:07:02 -0300

    refactor/0/ Reestructuracion del proyecto, separando clases por funcionalidad, desacoplamiento de responsabilidades y correccion de dependencias

---
+++ admin_banco/src/main/java/Interfaces/InterfazMenu.java
@@ -0,0 +1,84 @@
+package org.example;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class InterfazMenu {
+
+    private Banco banco;
+    private Proxy proxy;
+    private Scanner sc = new Scanner(System.in);
+    Director director = new Director();
+    CuentaBuilder builder = new CuentaBuilder();
+    //Admin admin = new Admin("Hector", 1);
+
+    //private ArrayList<Cuenta> listaCuenta = new ArrayList<>();
+
+    public void iniciar(){
+        int opcion = 0;
+
+        while(opcion != 5){
+            imprimirMenu();
+            opcion = sc.nextInt();
+            opciones(opcion);
+        }
+    }
+    private void imprimirMenu(){
+        System.out.println("\\--- APP BANCO ---");
+        System.out.println("1. Crear cuenta");
+        System.out.println("2. Loguearse");
+        System.out.println("3. Salir");
+        System.out.println("Seleccione una opción");
+    }
+
+    private void opciones(int opcion){
+        switch (opcion){
+            case 1:
+                System.out.println("Ingrese nombre");
+                String nombre = sc.nextLine();
+                System.out.println("Ingrese direccion");
+                String direc = sc.nextLine();
+                System.out.println("Ingrese contraseña");
+                String pass = sc.nextLine();
+                System.out.println("Ingrese tipo de cuenta \\ 1. Ahorro   2. Corriente");
+                int optipo = sc.nextInt();
+                if (optipo == 1 || optipo == 2){
+                    if (optipo == 1){
+                        director.constructCuentaAhorro(builder, nombre, direc, pass);
+                        Cuenta usuario = builder.getCuenta();
+                        banco.añadirCuenta(usuario);
+                        System.out.println("Creaste tu cuenta de ahorro con exito.");
+                    }
+                    else{
+                        director.constructCuentaCorriente(builder, nombre, direc, pass);
+                        Cuenta usuario = builder.getCuenta();
+                        banco.añadirCuenta(usuario);
+                        System.out.println("Creaste tu cuenta corriente con exito.");
+                    }
+                }
+                else
+                    System.out.println("Opcion Incorrecta");
+                break;
+            case 2:
+                System.out.println("Ingrese usuario");
+                String usuario = sc.nextLine();
+                System.out.println("Ingrese contraseña");
+                String password = sc.nextLine();
+                Cuenta sesionactual = proxy.autenticacion(usuario, password);
+                if (sesionactual != null){
+                    switch (sesionactual.rol){
+                        case (CLIENTE):
+
+                    }
+                }
+
+
+
+
+        }
+
+    }
+
+
+
+}
+
